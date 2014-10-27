7fce0720f71b98b405491a333e26d26fbfdca763
Prelang Builder <builder@prelang.com>
2014-10-27 13:20:57 +0000

    Generated model Workshop with migration

---
+++ db/migrate/20141027132056_create_workshops.rb
@@ -0,0 +1,14 @@
+class CreateWorkshops < ActiveRecord::Migration
+  def change
+    create_table :workshops do |t|
+      t.references :profile, index: true
+      t.string :title
+      t.string :description
+      t.string :picture_url
+      t.string :link_url
+      t.datetime :deleted_at
+
+      t.timestamps
+    end
+  end
+end
