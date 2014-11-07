15ccdc224bc32a54c2f68e6e193a9caf9d8139a3
Hudson Kelly <hk@hudsonkelly.xyz>
2014-11-07 10:47:33 -0500

    Learning piece controller and model to accept multiple topics

--- app/models/learning_piece.rb
+++ app/models/learning_piece.rb
@@ -2,4 +2,5 @@ class LearningPiece < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :topics, join_table: 'learning_pieces_topics'
   mount_uploader :avatar, AvatarUploader
+  accepts_nested_attributes_for :topics
 end
