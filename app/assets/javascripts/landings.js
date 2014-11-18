18be57661df79db1ec119603eca838ca4c983bcc
Hudson Kelly <hk@hudsonkelly.xyz>
2014-11-17 23:48:00 -0500

    Fix scroll arrows

--- app/assets/javascripts/landings.js
+++ app/assets/javascripts/landings.js
@@ -2,68 +2,61 @@
  * Created by Tribune on 11/3/2014.
  */
 
-var scrollSpeed = 3;
-var scrollInterval = 10;
-var ScrollIntID = 0;
-var ScrollObject;
+// All variables moved into DocumentReady to keep them out of the global namespace
+function DocumentReady() {
 
+    var scrollSpeed = 6;
+    // Upped this slightly hoping for a lighter CPU load
+    var scrollInterval = 16;
+    var scrollIntID = 0;
 
+    function ResizeListContainers() {
 
-function ResizeListContainers() {
+        var itemWidth = $(".sidescrollable-item").outerWidth(true);
+        var totalWidth = $(".learning-piece-item").length * itemWidth;
+        $(".learning-piece-wrapper .sidescrollable-container").width(totalWidth);
+        totalWidth = $(".consultant-item").length * itemWidth;
+        $(".consultants-wrapper .sidescrollable-container").width(totalWidth);
+        totalWidth = $(".product-item").length * itemWidth;
+        $(".products-wrapper .sidescrollable-container").width(totalWidth);
 
-    var itemWidth = $(".sidescrollable-item").outerWidth(true);
-    var totalWidth = $(".learning-piece-item").length * itemWidth;
-    $(".learning-piece-wrapper .sidescrollable-container").width(totalWidth);
-    totalWidth = $(".consultant-item").length * itemWidth;
-    $(".consultants-wrapper .sidescrollable-container").width(totalWidth);
-    totalWidth = $(".product-item").length * itemWidth;
-    $(".products-wrapper .sidescrollable-container").width(totalWidth);
+    }
 
-}
+    function onScrollButtonHover(evt) {
+        // Get the activated arrow object
+        var $arrow = $(evt.currentTarget);
+        // .data('dir') gets "data-dir" attribute from html object
+        var dir = $arrow.data('dir');
+        // Get the wrapper the arrow belongs to
+        var $wrapper = $arrow.closest(".sidescrollable-wrapper");
+        // Get the scroll container within the wrapper
+        var $scrollWrap = $wrapper.find('.sidescrollable-scrollwrap');
+        // Scroll the wrapper with paramaters
+        ScrollWrapper($scrollWrap, scrollSpeed, dir);
+    }
 
-function ScrollWrapperRight() {
-    ScrollObject.closest(".sidescrollable-wrapper").scrollLeft(ScrollObject.closest(".sidescrollable-wrapper").scrollLeft() + scrollSpeed);
-}
+    function offScrollButtonHover() {
+        clearInterval(scrollIntID);
+    }
 
-function ScrollWrapperLeft(ScrollWrapper) {
-    ScrollObject.closest(".sidescrollable-wrapper").scrollLeft(ScrollObject.closest(".sidescrollable-wrapper").scrollLeft() - scrollSpeed);
-}
+    function ScrollWrapper($wrapper, speed, dir) {
+        // Get the direction of the arrow as -1 or 1
+        dir = dir.toLowerCase() === 'left' ? -1 : 1;
+        // Scroll a wrapper
+        scrollIntID = setInterval(function () {
+            $wrapper.scrollLeft($wrapper.scrollLeft() + speed * dir);
+        }, scrollInterval);
+    }
 
-
-function WindowReady()
-{
     ResizeListContainers();
 
-    //------ This Indescriminately asigns scroll buttons to cause heir hover event to try and scroll their parent with the name ".sidescrollable-wrapper" ----------------
-    $(".scroll-btn-right").hover(function() {
-            if(ScrollIntID == 0) {
-                ScrollObject = $(this);
-                ScrollIntID = setInterval(ScrollWrapperRight, scrollInterval);
-                //ScrollWrapperRight($(this));
-            }
-        } , function() {
-            ScrollObject = null;
-            clearInterval(ScrollIntID);
-            ScrollIntID = 0;
-    });
+    //------ This Indescriminately asigns scroll buttons to cause heir hover event to try and scroll their parent with the name ".sidescrollable-wrapper" --------------
+    $(".scroll-btn").hover(onScrollButtonHover, offScrollButtonHover);
 
-    $(".scroll-btn-left").hover(function() {
-        if(ScrollIntID == 0) {
-            ScrollObject = $(this);
-            ScrollIntID = setInterval(ScrollWrapperLeft, scrollInterval);
-            //ScrollWrapperRight($(this));
-        }
-    } , function() {
-        ScrollObject = null;
-        clearInterval(ScrollIntID);
-        ScrollIntID = 0;
-    });
-
+    $(window).on('resize', ResizeListContainers);
 }
 
-
-
-$(window).on('resize', ResizeListContainers);
-$(window).ready(WindowReady);
+// Changed to document.ready because that's what people use
+$(document).ready(DocumentReady);
 
 
